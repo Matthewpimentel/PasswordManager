f2e5307df1cd6cce06104396d376b4834745ac84
Matthew Pimentel <PimentelMatthew@outlook.com>
2022-06-18 11:43:24 -0400

    Basic functionality is added (search, add pw)

--- PasswordManager/MainWindow.xaml.cs
+++ PasswordManager/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace PasswordManager
 {
@@ -20,9 +23,88 @@ namespace PasswordManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Password> pw = new List<Password>();
+        List<Password> hold = new List<Password>();
+        string path = "passwords.json";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(path))
+            {
+               using (StreamReader r = new StreamReader(path))
+                {
+                    string json = r.ReadToEnd();
+                    pw = JsonSerializer.Deserialize<List<Password>>(json);
+                }
+                passwords.ItemsSource = pw;
+                passwords.Items.Refresh();
+
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var program = programName.Text;
+            var user = userName.Text;
+            var password = passwordInput.Text;
+
+            if(program != null && user != null && password != null)
+            {
+                pw.Add(new Password(program, user, password));
+                passwords.ItemsSource = pw;
+                passwords.Items.Refresh();
+
+                programName.Clear();
+                userName.Clear();
+                passwordInput.Clear();
+            }    
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            List<Password> search = new List<Password>();
+
+            for(int i = 0; i < pw.Count; i++)
+            {
+                if (pw[i].Program.ToUpper().Contains(searchBox.Text.ToUpper()))
+                {
+                    search.Add(pw[i]);
+                    passwords.ItemsSource = search;
+                    passwords.Items.Refresh();
+                }
+            }
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+     
+                StreamWriter tw = new StreamWriter(path);
+      
+                string listToString = JsonSerializer.Serialize(pw);
+
+                if (pw.Count > 0)
+                {
+                    tw.WriteLine(listToString);
+                }
+
+                tw.Close();
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(passwords.SelectedItem != null)
+            {
+               
+                
+               
+            }
         }
     }
 }
---
+++ PasswordManager/Password.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager
+{
+    internal class Password
+    {
+        public string Program { get; set; }
+        public string Username { get; set; }
+        public string Pw { get; set; }
+
+        public Password(string program, string username, string pw)
+        {
+            Program = program;
+            Username = username;
+            Pw = pw;
+        }
+    }
+}
